ac8397fa3dae162399346b64339999a2f9f300c9
Jashanpreet singh <79403733+Jashanpsingh@users.noreply.github.com>
2022-10-17 23:58:04 +0530

    Add files via upload

---
+++ Basketball page/basketball page.js
@@ -0,0 +1,56 @@
+      $(document).ready(function(){
+        $(".link1").mouseover(function(){
+          $(".link1").css("color","white");
+        });
+        $(".link2").mouseover(function(){
+          $(".link2").css("color","white");
+        });
+        $(".link3").mouseover(function(){
+          $(".link3").css("color","white");
+        });
+        $(".link4").mouseover(function(){
+          $(".link4").css("color","white");
+        });
+        $(".link1").mouseout(function(){
+          $(".link1").css("color","cyan");
+        });
+        $(".link2").mouseout(function(){
+          $(".link2").css("color","cyan");
+        });
+        $(".link3").mouseout(function(){
+          $(".link3").css("color","cyan");
+        });
+        $(".link4").mouseout(function(){
+          $(".link4").css("color","cyan");
+        });
+      })
+      function addCart()
+      {
+        window.alert("Item is added to cart"); onclick="addToCart()"
+      }
+      $(document).ready(function(){
+        $(".buy-now").click(function(){
+          $("#myModal1").modal({backdrop: true});
+        });
+      })
+
+      $(document).ready(function(){
+        $("#send").click(function(){
+          alert("Your order has been placed");
+        });
+      })
+      function addCart(){
+        var count = document.getElementById('totalCount').innerHTML;
+        alert("Item has been added to Cart");
+        document.getElementById('totalCount').innerHTML= Number(count)+1;
+      }
+
+function Delivery(){
+    alert("Standard Delivery Charges:- INR 40")
+}
+function payment(){
+    alert("You can Pay online as well as COD Avilable...")
+}
+function Return(){
+    alert("No Return Avilable....")
+}
